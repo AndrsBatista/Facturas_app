b7b47d0f588e47a3d9cc55573f0b3cabdfad9dfb
Andrés Batista <andrs.batistav02@gmail.com>
2026-01-12 15:06:32 -0400

    Update database settings to use environment variables

--- Factura/settings.py
+++ Factura/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'Factura.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'facturasdb',
-        'USER': 'postgres',
-        'PASSWORD': 'Caasd21215@@',
-        'HOST': 'db', 
-        'PORT': '5432',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'), 
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
